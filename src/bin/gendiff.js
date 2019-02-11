368f2df0881ae1605efc528b19086758be194953
euhoo <euhoo@mail.ru>
2019-02-11 07:09:31 +0300

    make help

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,33 +1,11 @@
 #!/usr/bin/env node
-// import makeHelp from '../files/calc';
-import commander from 'commander';
-
-const program = require('commander');
+import program from 'commander';
 
 program
-  .version('0.0.1', '-v, --version')
-  .description(`Usage: gendiff [options] <firstConfig> <secondConfig>
+  .version('1.0.8', '-V, --version')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'Output format')
+    
+program.parse(process.argv);
 
-  Compares two configuration files and shows a difference.
-
-  Options:
-
-    -h, --help           output usage information
-    -V, --version        output the version number
-    -f, --format [type]  Output format`)
-  .option('-h, --help', program.description)
-  .option('-V, --version', 'output the version number')
-  .option('-f, --format [type]', 'Output format [type]', 'type')
-  .parse(process.argv);
-
-if (program.help) {console.log(`Usage: gendiff [options] <firstConfig> <secondConfig>
-
-Compares two configuration files and shows a difference.
-
-Options:
-
-  -h, --help           output usage information
-  -V, --version        output the version number
-  -f, --format [type]  Output format`);}
-if (program.version) console.log(program.version);
-if (program.format) console.log('  - % format', program.format);
+if (!program.args.length) program.help();
