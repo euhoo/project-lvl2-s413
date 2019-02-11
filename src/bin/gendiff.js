7076558dae2a7f53dd79b5fff09c9433bbfcfda3
euhoo <euhoo@mail.ru>
2019-02-11 07:23:49 +0300

    fix eslint notes

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -4,8 +4,7 @@ import program from 'commander';
 program
   .version('1.0.8', '-V, --version')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format [type]', 'Output format')
-    
+  .option('-f, --format [type]', 'Output format');
 program.parse(process.argv);
 
 if (!program.args.length) program.help();
