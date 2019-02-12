8d49ee40dea7ec8a7016f97528e4c9b4bc825b1c
euhoo <euhoo@mail.ru>
2019-02-12 19:35:39 +0300

    4

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -3,11 +3,7 @@ import program from 'commander';
 
 program
   .version('1.0.27', '-V, --version')
-  .description('Compares two configuration files and shows a difference.')
   .arguments('<first> <second>')
+  .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'Output format');
 program.parse(process.argv);
-
-if (!program.args.length) program.help();
-
-console.log('Well done');
