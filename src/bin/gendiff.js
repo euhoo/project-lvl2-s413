d3344d52f9c31a26e4790965a96c23c6e80dfe21
euhoo <euhoo@mail.ru>
2019-02-12 19:08:22 +0300

    1 stage

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,16 +1,11 @@
 #!/usr/bin/env node
 import program from 'commander';
-import getDiff from '../files/makeDifference';
 
 program
-  .version('1.0.8', '-V, --version')
+  .version('1.0.27', '-V, --version')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<first> <second>')
   .option('-f, --format [type]', 'Output format');
 program.parse(process.argv);
 
 if (!program.args.length) program.help();
-
-export default (before, after) => {
-  getDiff(before, after);
-};
--- src/files/makeDifference.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-import { makeArr, makeStr } from '..';
-
-export default (before, after) => {
-  const arr = makeArr(before, after);
-  const str = makeStr(arr);
-  return str;
-};
