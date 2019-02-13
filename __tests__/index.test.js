ff46e70a0ae0f4bfafd3399af8966015fe663c50
euhoo <euhoo@mail.ru>
2019-02-13 19:55:16 +0300

    add yml test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,9 +1,26 @@
 import genDiff from '../src';
 
-const beforePath = '__tests__/__fixtures__/before.json';
-const afterPath = '__tests__/__fixtures__/after.json';
 
-test('Test1', () => {
+test('Test 1: Json', () => {
+  const beforePath = '__tests__/__fixtures__/before.json';
+  const afterPath = '__tests__/__fixtures__/after.json';
+  const received = genDiff(beforePath, afterPath);
+  // console.log(`Мой вариант:\n ${received}`);
+
+  const expected = `{
+    host: hexlet.io
+  - timeout: 50
+  + timeout: 20
+  - proxy: 123.234.53.22
+  - follow: false
+  + verbose: true
+}`;
+  // console.log(`Тесты:\n ${expected}`);
+  expect(received).toEqual(expected);
+});
+test('Test 2: Yaml', () => {
+  const beforePath = '__tests__/__fixtures__/before.yml';
+  const afterPath = '__tests__/__fixtures__/after.yml';
   const received = genDiff(beforePath, afterPath);
   // console.log(`Мой вариант:\n ${received}`);
 
