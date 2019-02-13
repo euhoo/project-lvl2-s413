3752790cd9ca4ab0d551ec79b3b0d046de089c7d
euhoo <euhoo@mail.ru>
2019-02-13 08:29:22 +0300

    fix logic and tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -8,12 +8,12 @@ test('If both path\'s correct ', () => {
   // console.log(`Мой вариант:\n ${received}`);
 
   const expected = `{
-  + timeout: 20
-  - timeout: 50
-  + verbose: true
     host: hexlet.io
+  - timeout: 50
+  + timeout: 20
   - proxy: 123.234.53.22
   - follow: false
+  + verbose: true
 }`;
   // console.log(`Тесты:\n ${expected}`);
   expect(received).toEqual(expected);
--- src/index.js
+++ src/index.js
@@ -1,30 +1,28 @@
 import { has } from 'lodash';
 import fs from 'fs';
 
-const getStrFromFile = pathToFile => fs.readFileSync(pathToFile, 'utf-8');
+const makeStrFromFile = pathToFile => fs.readFileSync(pathToFile, 'utf-8');
 
 const makeArr = (before, after) => {
-  const total = Object.assign({}, before, after);
-  const keys = Object.keys(total);
+  const unionOfObjects = Object.assign({}, before, after);
+  const keys = Object.keys(unionOfObjects);
   return keys.reduce((acc, key) => {
     if (has(before, key) && has(after, key)) {
       return (before[key] === after[key]) ? [...acc, { name: before[key], key, operation: ' ' }]
-        : [...acc, { name: before[key], key, operation: '+' }, { name: after[key], key, operation: '-' }];
+        : [...acc, { name: before[key], key, operation: '-' }, { name: after[key], key, operation: '+' }];
     }
-    if (has(after, key)) return [...acc, { name: after[key], key, operation: '-' }];
-    return [...acc, { name: before[key], key, operation: '+' }];
+    if (has(after, key)) return [...acc, { name: after[key], key, operation: '+' }];
+    return [...acc, { name: before[key], key, operation: '-' }];
   }, []);
 };
-const makeResult = (arr) => {
+const makeStringFromArray = (arr) => {
   const resultString = arr.reduce((acc, obj) => `${acc}  ${obj.operation} ${obj.key}: ${obj.name}\n`, '');
   return `{\n${resultString}}`;
 };
 
-export default (fileBefore, fileAfter) => {
-  const strBefore = getStrFromFile(fileBefore);
-  const strAfter = getStrFromFile(fileAfter);
-  const objBefore = JSON.parse(strBefore);
-  const objAfter = JSON.parse(strAfter);
+export default (filePathBefore, filePathAfter) => {
+  const [strBefore, strAfter] = [makeStrFromFile(filePathBefore), makeStrFromFile(filePathAfter)];
+  const [objBefore, objAfter] = [JSON.parse(strBefore), JSON.parse(strAfter)];
   const arr = makeArr(objBefore, objAfter);
-  return makeResult(arr);
+  return makeStringFromArray(arr);
 };
