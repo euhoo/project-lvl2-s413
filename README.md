4daf5577d2908de965a83ac7cf96da6799387665
euhoo <euhoo@mail.ru>
2019-02-14 00:19:43 +0300

    add asciinema and refactoring tests

--- README.md
+++ README.md
@@ -2,4 +2,4 @@ it's my second project
 [![Maintainability](https://api.codeclimate.com/v1/badges/23e1336527d58dbfc833/maintainability)](https://codeclimate.com/github/euhoo/project-lvl2-s413/maintainability) 
 [![Test Coverage](https://api.codeclimate.com/v1/badges/23e1336527d58dbfc833/test_coverage)](https://codeclimate.com/github/euhoo/project-lvl2-s413/test_coverage) 
 [![Build Status](https://travis-ci.org/euhoo/project-lvl2-s413.svg?branch=master)](https://travis-ci.org/euhoo/project-lvl2-s413)  
-[![asciicast](https://asciinema.org/a/Yxprx9sYRjgekMsH3ZMB5xn3F.svg)](https://asciinema.org/a/Yxprx9sYRjgekMsH3ZMB5xn3F)
+[![asciicast](https://asciinema.org/a/cGgy5uQJ7XtCHGSnD39RN7orz.svg)](https://asciinema.org/a/cGgy5uQJ7XtCHGSnD39RN7orz)
--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,13 +1,10 @@
 import fs from 'fs';
 import genDiff from '../src';
 
-const expectedPath = '__tests__/__fixtures__/expected.txt';
-const beforePath = '__tests__/__fixtures__/before.';
-const afterPath = '__tests__/__fixtures__/after.';
-const expected = fs.readFileSync(expectedPath, 'utf-8');
-const tests = ['Json', 'Yml', 'Ini'];
-const func = element => test(`Test : ${element}`, () => {
-  const received = genDiff(`${beforePath}${element.toLowerCase()}`, `${afterPath}${element.toLowerCase()}`);
-  expect(expected).toBe(received);
+const path = '__tests__/__fixtures__/';
+const types = ['Json', 'Yml', 'Ini'];
+
+test.each(types)('Test %s', (element) => {
+  const received = genDiff(`${path}before.${element.toLowerCase()}`, `${path}after.${element.toLowerCase()}`);
+  expect(fs.readFileSync(`${path}expected.txt`, 'utf-8')).toBe(received);
 });
-tests.forEach(func);
