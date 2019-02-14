c927d50468dc3ab05238e2eea63c647111491fe7
euhoo <euhoo@mail.ru>
2019-02-15 00:42:33 +0300

    adds

--- src/render.js
+++ src/render.js
@@ -3,7 +3,7 @@ import normalize from './utils';
 const render = (arr) => {
   const str = arr.reduce((acc, obj) => {
     const value = normalize(obj.value);
-    return `${acc}  ${obj.status} ${obj.key}: ${value}\n`;
+    return `${acc} ${'  '.repeat(0)} ${obj.status} ${obj.key}: ${value}\n`;
   }, '');
   return `{\n${str}}`;
 };
