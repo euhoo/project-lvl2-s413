d8a7de0c42f32adbd48c2f86167f1d1492d636a3
euhoo <euhoo@mail.ru>
2019-02-15 00:31:50 +0300

    make utils

--- src/render.js
+++ src/render.js
@@ -1,17 +1,8 @@
-const render = (arr) => {
-  const arrayToString = value => (Array.isArray(value) ? render(value) : value);
+import normalize from './utils';
 
-  const normnormalize = (value) => {
-    if ((typeof value === 'object')) {
-      const keys = Object.keys(value);
-      const str = keys.reduce((acc, key) => `${acc}\n    ${key}: ${value[key]}`, '');
-      return `{${str}\n}`;
-    }
-    return value;
-  };
+const render = (arr) => {
   const str = arr.reduce((acc, obj) => {
-    const value1 = arrayToString(obj.value);
-    const value = normnormalize(value1);
+    const value = normalize(obj.value);
     return `${acc}  ${obj.status} ${obj.key}: ${value}\n`;
   }, '');
   return `{\n${str}}`;
