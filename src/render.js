6fcc5bf71f7a78bb7d2279336b434438737e0cd1
euhoo <euhoo@mail.ru>
2019-02-15 09:33:19 +0300

    render clean

--- src/render.js
+++ src/render.js
@@ -1,23 +1,16 @@
-
-
 const render = (arr, depth = 1) => {
-  const arrayToString = value => (Array.isArray(value) ? render(value, depth + 2) : value);
-
   const normalize = (data) => {
-    const value = arrayToString(data);
+    const value = Array.isArray(data) ? render(data, depth + 2) : data;
     if ((typeof value === 'object')) {
       const keys = Object.keys(value);
-      const str = keys.reduce((acc, key) => `${acc}${'  '.repeat(depth + 2)}  ${key}: ${value[key]}`, '');
+      const str = keys.map(key => `${'  '.repeat(depth + 2)}  ${key}: ${value[key]}`);
       return `{\n${str}\n${'  '.repeat(depth + 1)}}`;
     }
     return value;
   };
+  const str = arr.reduce((acc, obj) => `${acc}${'  '.repeat(depth)}${obj.status} ${obj.key}: ${normalize(obj.value)}\n`,
+    '');
 
-  const str = arr.reduce((acc, obj) => {
-    const value = normalize(obj.value);
-    return `${acc}${'  '.repeat(depth)}${obj.status} ${obj.key}: ${''.repeat(depth)}${value}\n`;
-  }, '');
   return `{\n${str}${' '.repeat(depth * 2 - 2)}}`;
 };
-
 export default render;
