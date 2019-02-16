b815e92c92ef39f70b3dbb4215326f455fb1821b
euhoo <euhoo@mail.ru>
2019-02-16 08:55:36 +0300

    naming and  fix renderer tree

--- src/renderers/tree.js
+++ src/renderers/tree.js
@@ -1,29 +1,27 @@
+const getCorrectValue = (data, func, depth) => {
+  const value = Array.isArray(data) ? func(data, depth + 2) : data;
+  if ((typeof value === 'object')) {
+    const keys = Object.keys(value);
+    const str = keys.map(key => `${'  '.repeat(depth + 2)}  ${key}: ${value[key]}`);
+    return `{\n${str}\n${'  '.repeat(depth + 1)}}`;
+  }
+  return value;
+};
+const getValue = (oldValue = '', newValue = '', obj) => (obj.status === 'changed' || obj.status === 'added' ? newValue : oldValue);
+
+const statusObj = {
+  unchanged: ' ',
+  nested: ' ',
+  added: '+',
+  deleted: '-',
+  changed: '+',
+  deletedWhenChanged: '-',
+};
 const renderTree = (arr, depth = 1) => {
-  const normalize = (data) => {
-    const value = Array.isArray(data) ? renderTree(data, depth + 2) : data;
-    if ((typeof value === 'object')) {
-      const keys = Object.keys(value);
-      const str = keys.map(key => `${'  '.repeat(depth + 2)}  ${key}: ${value[key]}`);
-      return `{\n${str}\n${'  '.repeat(depth + 1)}}`;
-    }
-    return value;
-  };
-  const statusObj = {
-    unchanged: ' ',
-    nested: ' ',
-    added: '+',
-    deleted: '-',
-    changed: '+',
-    deletedWhenChanged: '-',
-  };
   const str = arr.reduce((acc, obj) => {
-    const getValue = (oldValue = '', newValue = '') => {
-      if (obj.status === 'changed' || obj.status === 'added') return newValue;
-      // if (obj.status === 'deletedWhenChanged' ||
-      // obj.status === 'unchanged' || obj.status === 'deleted')
-      return oldValue;
-    };
-    return `${acc}${'  '.repeat(depth)}${statusObj[obj.status]} ${obj.key}: ${Array.isArray(obj.children) ? normalize(obj.children) : normalize(getValue(obj.oldValue, obj.newValue))}\n`;
+    const dirtyValue = obj.children || getValue(obj.oldValue, obj.newValue, obj);
+    const value = getCorrectValue(dirtyValue, renderTree, depth);
+    return `${acc}${'  '.repeat(depth)}${statusObj[obj.status]} ${obj.key}: ${value}\n`;
   }, '');
 
   return `{\n${str}${' '.repeat(depth * 2 - 2)}}`;
