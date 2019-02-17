3324f996e9914d8bc7d219efc0b09e5db3fbf4b9
euhoo <euhoo@mail.ru>
2019-02-17 21:30:58 +0300

    step6

--- src/renderers/plain.js
+++ src/renderers/plain.js
@@ -1,22 +1,23 @@
+import _ from 'lodash';
+
 const makeQuotes = (value) => {
   if (value instanceof Object) return '[complex value]';
   if (typeof value === 'boolean') return value;
   return `'${value}'`;
 };
-const checkPoint = parent => ((parent.length === 0) ? '' : '.');
-const checkNested = (obj, func) => (obj.type === 'nested' ? func(obj.children, obj.key) : obj.key);
-const renderPlain = (ast, parent) => ast
-  .filter(obj => obj.type !== 'unchanged')
-  .map((obj) => {
-    const { type } = obj;
-    const typesObj = {
-      changed: `${parent}${checkPoint(parent)}${obj.key}' was updated. From ${makeQuotes(obj.oldValue)} to ${makeQuotes(obj.newValue)}`,
-      unchanged: '',
-      nested: `${parent}${checkPoint(parent)}${checkNested(obj, renderPlain)}`,
-      deleted: `${parent}${checkPoint(parent)}${obj.key}' was removed`,
-      added: `${parent}${checkPoint(parent)}${obj.key}' was added with value: ${makeQuotes(obj.newValue)}`,
-    };
-    return typesObj[type];
-  })
-  .join('\nProperty \'');
-export default (ast, parent = '') => `Property '${renderPlain(ast, parent)}`;
+
+const typesObj = {
+  changed: (obj, parent) => `Property '${[...parent, obj.key].join('.')}' was updated. From ${makeQuotes(obj.oldValue)} to ${makeQuotes(obj.newValue)}`,
+  unchanged: '',
+  nested: (obj, parent, func) => _.flatten(func(obj.children, [...parent, obj.key])),
+  deleted: (obj, parent) => `Property '${[...parent, obj.key].join('.')}' was removed`,
+  added: (obj, parent) => `Property '${[...parent, obj.key].join('.')}' was added with value: ${makeQuotes(obj.newValue)}`,
+};
+
+export default (data) => {
+  const renderPlain = (item, parent = []) => item
+    .filter(obj => obj.type !== 'unchanged')
+    .map(el => typesObj[el.type](el, parent, renderPlain));
+  return _.flatten(renderPlain(data))
+    .join('\n');
+};
