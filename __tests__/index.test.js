9339635d4694dab359e195010e47a0099265bb62
euhoo <euhoo@mail.ru>
2019-02-17 19:32:45 +0300

    make best tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,23 +3,15 @@ import genDiff from '../src';
 
 const path = '__tests__/__fixtures__/';
 const types = ['Json', 'Yml', 'Ini'];
+const format = ['Flatten', 'Tree', 'FlattenPlain', 'Plain'];
+const obj = {
+  Flatten: 'tree',
+  Tree: 'tree',
+  FlattenPlain: 'plain',
+  Plain: 'plain',
+};
 
-test.each(types)('Test %s', (element) => {
-  const received = genDiff(`${path}before.${element.toLowerCase()}`, `${path}after.${element.toLowerCase()}`);
-  expect(fs.readFileSync(`${path}expected.txt`, 'utf-8')).toBe(received);
-});
-
-test.each(types)('Test tree %s', (element) => {
-  const received = genDiff(`${path}beforeTree.${element.toLowerCase()}`, `${path}afterTree.${element.toLowerCase()}`);
-  expect(fs.readFileSync(`${path}expectedTree.txt`, 'utf-8')).toBe(received);
-});
-
-test.each(types)('Test Flatten Plain %s', (element) => {
-  const received = genDiff(`${path}before.${element.toLowerCase()}`, `${path}after.${element.toLowerCase()}`, 'plain');
-  expect(fs.readFileSync(`${path}expectedFlattenPlain.txt`, 'utf-8')).toBe(received);
-});
-
-test.each(types)('Test Plain %s', (element) => {
-  const received = genDiff(`${path}beforeTree.${element.toLowerCase()}`, `${path}afterTree.${element.toLowerCase()}`, 'plain');
-  expect(fs.readFileSync(`${path}expectedPlain.txt`, 'utf-8')).toBe(received);
-});
+format.forEach(item => test.each(types)(`Test ${item} %s`, (element) => {
+  const received = genDiff(`${path}before${item}.${element.toLowerCase()}`, `${path}after${item}.${element.toLowerCase()}`, `${obj[item]}`);
+  expect(fs.readFileSync(`${path}expected${item}.txt`, 'utf-8')).toBe(received);
+}));
--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -3,7 +3,7 @@ import program from 'commander';
 import genDiff from '..';
 
 program
-  .version('1.0.27', '-V, --version')
+  .version('1.3.7', '-V, --version')
   .arguments('<first> <second>')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'Output format')
