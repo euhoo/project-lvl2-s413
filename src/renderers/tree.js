b05557e8f1d99e4da72759cb1c0c6a67af2b951a
euhoo <euhoo@mail.ru>
2019-02-17 13:42:54 +0300

    fix

--- src/renderers/tree.js
+++ src/renderers/tree.js
@@ -4,18 +4,18 @@ const getCorrectValue = (value, depth) => {
   if (!(typeof value === 'object')) return value;
   const keys = Object.keys(value);
   const result = keys.map(key => `${'    '.repeat(depth + 1)}${key}: ${value[key]}`);
-  return [`{\n${result.join('\n')}\n${'    '.repeat(depth)}}`];
+  return `{\n${result.join('\n')}\n${'    '.repeat(depth)}}`;
 };
 
 const renderTree = (ast, depth = 0) => {
   const makeStr = (operation, key, value) => `${'  '.repeat(depth * 2 + 1)}${operation} ${key}: ${value}`;
 
   const typesObj = {
-    unchanged: obj => [makeStr(' ', obj.key, getCorrectValue(obj.oldValue, depth + 1))],
-    nested: obj => [makeStr(' ', obj.key, renderTree(obj.children, depth + 1))],
-    added: obj => [makeStr('+', obj.key, getCorrectValue(obj.newValue, depth + 1))],
-    deleted: obj => [makeStr('-', obj.key, getCorrectValue(obj.oldValue, depth + 1))],
-    changed: obj => [[typesObj.added(obj)], [typesObj.deleted(obj)]],
+    unchanged: obj => makeStr(' ', obj.key, getCorrectValue(obj.oldValue, depth + 1)),
+    nested: obj => makeStr(' ', obj.key, renderTree(obj.children, depth + 1)),
+    added: obj => makeStr('+', obj.key, getCorrectValue(obj.newValue, depth + 1)),
+    deleted: obj => makeStr('-', obj.key, getCorrectValue(obj.oldValue, depth + 1)),
+    changed: obj => [typesObj.added(obj), typesObj.deleted(obj)],
   };
   const mapped = ast.map(obj => typesObj[obj.type](obj));
   const result = _
