9b503fa7994846d6bb2ac92f1ca01caec7a4d813
euhoo <euhoo@mail.ru>
2019-02-17 19:44:45 +0300

    made better tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,15 +3,21 @@ import genDiff from '../src';
 
 const path = '__tests__/__fixtures__/';
 const types = ['Json', 'Yml', 'Ini'];
-const format = ['Flatten', 'Tree', 'FlattenPlain', 'Plain'];
-const obj = {
+const waysOfTest = ['Flatten', 'Tree', 'FlattenPlain', 'Plain'];
+const testsFormat = {
   Flatten: 'tree',
   Tree: 'tree',
   FlattenPlain: 'plain',
   Plain: 'plain',
 };
+const fileName = {
+  Flatten: 'Flatten',
+  FlattenPlain: 'Flatten',
+  Tree: 'Tree',
+  Plain: 'Tree',
+};
 
-format.forEach(item => test.each(types)(`Test ${item} %s`, (element) => {
-  const received = genDiff(`${path}before${item}.${element.toLowerCase()}`, `${path}after${item}.${element.toLowerCase()}`, `${obj[item]}`);
+waysOfTest.forEach(item => test.each(types)(`Test ${item} %s`, (element) => {
+  const received = genDiff(`${path}before${fileName[item]}.${element.toLowerCase()}`, `${path}after${fileName[item]}.${element.toLowerCase()}`, `${testsFormat[item]}`);
   expect(fs.readFileSync(`${path}expected${item}.txt`, 'utf-8')).toBe(received);
 }));
