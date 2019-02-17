d132bbfd3a3ace626c58c947753292527293756e
euhoo <euhoo@mail.ru>
2019-02-17 11:06:45 +0300

    some fix

--- src/renderers/tree.js
+++ src/renderers/tree.js
@@ -3,27 +3,25 @@ import _ from 'lodash';
 const getCorrectValue = (value, depth) => {
   if (!(typeof value === 'object')) return value;
   const keys = Object.keys(value);
-  const result = keys.map(key => `${' '.repeat(depth * 4 + 4)}${key}: ${value[key]}`);
-  return `{\n${result.join('\n')}\n${' '.repeat(depth * 4)}}`;
+  const result = keys.map(key => `${'    '.repeat(depth + 1)}${key}: ${value[key]}`);
+  return [`{\n${result.join('\n')}\n${'    '.repeat(depth)}}`];
 };
 
 const renderTree = (ast, depth = 0) => {
-  const makeStr = (operation, key, value) => `${' '.repeat(depth * 4 + 2)}${operation} ${key}: ${value}`;
+  const makeStr = (operation, key, value) => `${'  '.repeat(depth * 2 + 1)}${operation} ${key}: ${value}`;
 
   const allTypes = {
-    unchanged: obj => makeStr(' ', obj.key, getCorrectValue(obj.oldValue, depth + 1)),
-    nested: obj => makeStr(' ', obj.key, renderTree(obj.children, depth + 1)),
-    added: obj => makeStr('+', obj.key, getCorrectValue(obj.newValue, depth + 1)),
-    deleted: obj => makeStr('-', obj.key, getCorrectValue(obj.oldValue, depth + 1)),
-    changed: obj => [makeStr('+', obj.key, getCorrectValue(obj.newValue, depth + 1)), makeStr('-', obj.key, getCorrectValue(obj.oldValue, depth + 1))],
-    // changed: obj => [[allTypes.added(obj)], [allTypes.deleted(obj)]],
-    // попробовать это реализовать
+    unchanged: obj => [makeStr(' ', obj.key, getCorrectValue(obj.oldValue, depth + 1))],
+    nested: obj => [makeStr(' ', obj.key, renderTree(obj.children, depth + 1))],
+    added: obj => [makeStr('+', obj.key, getCorrectValue(obj.newValue, depth + 1))],
+    deleted: obj => [makeStr('-', obj.key, getCorrectValue(obj.oldValue, depth + 1))],
+    changed: obj => [[allTypes.added(obj)], [allTypes.deleted(obj)]],
   };
   const mapped = ast.map(obj => allTypes[obj.type](obj));
   const result = _
     .flatten(mapped)
     .join('\n');
-  return `{\n${result}\n${' '.repeat(depth * 4)}}`;
+  return `{\n${result}\n${'    '.repeat(depth)}}`;
 };
 
 export default renderTree;
