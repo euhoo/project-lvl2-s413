8034e593b918fe7a97444303cd813f2ea8eccffe
euhoo <euhoo@mail.ru>
2019-02-17 18:36:24 +0300

    okain renderer fix and little better naming

--- src/renderers/plain.js
+++ src/renderers/plain.js
@@ -4,20 +4,19 @@ const makeQuotes = (value) => {
   return `'${value}'`;
 };
 const checkPoint = parent => ((parent.length === 0) ? '' : '.');
-const getCorrectKey = (obj, func) => (obj.type === 'nested' ? func(obj.children, obj.key) : obj.key);
+const checkNested = (obj, func) => (obj.type === 'nested' ? func(obj.children, obj.key) : obj.key);
 const getStr = (ast, parent) => ast
   .filter(obj => obj.type !== 'unchanged')
   .map((obj) => {
     const { type } = obj;
     const typesObj = {
-      changed: [`${parent}${checkPoint(parent)}${getCorrectKey(obj, getStr)}' was updated. From ${makeQuotes(obj.oldValue)} to ${makeQuotes(obj.newValue)}`],
+      changed: `${parent}${checkPoint(parent)}${obj.key}' was updated. From ${makeQuotes(obj.oldValue)} to ${makeQuotes(obj.newValue)}`,
       unchanged: '',
-      nested: [`${parent}${checkPoint(parent)}${getCorrectKey(obj, getStr)}`],
-      deleted: [`${parent}${checkPoint(parent)}${getCorrectKey(obj, getStr)}' was removed`],
-      added: [`${parent}${checkPoint(parent)}${getCorrectKey(obj, getStr)}' was added with value: ${makeQuotes(obj.newValue)}`],
+      nested: `${parent}${checkPoint(parent)}${checkNested(obj, getStr)}`,
+      deleted: `${parent}${checkPoint(parent)}${obj.key}' was removed`,
+      added: `${parent}${checkPoint(parent)}${obj.key}' was added with value: ${makeQuotes(obj.newValue)}`,
     };
     return typesObj[type];
   })
-  .join('\nProperty \''); // присоединяю Property к каждой строчке,начиная со второй
-  // попробовать сделать,чтобы Property и кавычка в теле функции реализовывались
-export default (arr, parent = '') => `Property '${getStr(arr, parent)}`; // Присоединяю Property в начало и возвращаю функцию
+  .join('\nProperty \'');
+export default (arr, parent = '') => `Property '${getStr(arr, parent)}`;
